11ce2fc42a849995b9018c15ee9fe3b187610aab
joni mesfin <joni.mesfin@yahoo.com>
2022-12-08 18:29:38 -0800

    execute

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */ 
+int main(void)
+{
+	char putstring[100] = "Programming is like building a multilingual puzzle\n";
+	puts(putstring);
+	return (0);
+}
