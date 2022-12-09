59cad29dee371b4e4aa08b189b6e66b10f0b554a
joni mesfin <joni.mesfin@yahoo.com>
2022-12-08 19:07:22 -0800

    commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
+
 /**
  * main - Entry Point
  *
- * return - if returns 0 (Success)
+ * Return - Always (Success)
  */
 int main(void)
 {
+	int integerType;
+	long int longintType;
+	long long int longlongintType;
+	float floatType;
+	double doubleType;
+	char charType;
 
+	/*Sizeof operator is used to evaluate the size of a variable*/
+	printf("Size of a char: %ld byte(s)", sizeof(charType));
+	printf("Size of an int: %ld byte(s)", sizeof(integerType));
+	printf("Size of a long int: %ld byte(s)", sizeof(longintType));
+	printf("Size of a long long int: %ld byte(s)", sizeof(longlongintType));
+	printf("Size of a float: %ld byte(s)", sizeof(floatType));
+	return (0);
+}
