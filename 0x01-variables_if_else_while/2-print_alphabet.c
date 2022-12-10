b2ba6ba5ace507588480101a37db634e28047b01
jonimes <jonimsfn@gmail.com>
2022-12-10 05:43:16 +0300

    a-z

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 97;
+
+	for (a = 97; a < 123; a++)
+	{
+		putchar(a);
+	};
+	putchar('\n');
+	return (0);
+}
