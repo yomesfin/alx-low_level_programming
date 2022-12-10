384d8bf9e0f6adf5257b4b1fb135011fa5820a9a
jonimes <jonimsfn@gmail.com>
2022-12-10 05:51:11 +0300

    reverse 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int a = 122;
+
+	for (a = 122; a > 97; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+}
