2768f392ea957349813eba0470b54c4413281234
joni mesfin <joni.mesfin@yahoo.com>
2022-12-12 09:46:25 -0800

    live server

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 48;
+
+	while (i <= 57)
+	{
+		putchar (i);
+		i += 1;
+		if (i == 58)
+		{
+			break;
+		}
+		putchar (',');
+	}
+	return (0);
+}
