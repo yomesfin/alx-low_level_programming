5908f6b1397446392e52c975df9ba211ae05a360
jonimes <jonimsfn@gmail.com>
2022-12-13 06:52:48 +0300

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int arr[] = {95, 112, 117, 116, 99, 104, 97, 114};
+	int arrLen = sizeof arr / sizeof arr[0];
+	int i = 0;
+
+	while (i < arrLen)
+	{
+		putchar (arr[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
+
