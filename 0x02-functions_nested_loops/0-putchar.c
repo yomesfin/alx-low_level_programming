c3236c66e8985adafbbdeb20bda540c6ff343c82
jonimes <jonimsfn@gmail.com>
2022-12-13 07:01:48 +0300

    hot fix on betty

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int arr[] = {95, 112, 117, 116, 99, 104, 97, 114};
-	int arrLen = sizeof arr / sizeof arr[0];
+	int arrLen = sizeof(arr) / sizeof(arr[0]);
 	int i = 0;
 
 	while (i < arrLen)
