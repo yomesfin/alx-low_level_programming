086381a358b39fa7607929744eaf241c585e758c
jonimes <jonimsfn@gmail.com>
2022-12-16 07:28:29 +0300

    betty fix

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,11 @@
 #include "main.h"
 
+/**
+ * _isupper - checks for uppercase character
+ * @c: the character to check
+ * Return: 1 if c is upper, 0 otherwise
+ */
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z') ? 1 : 0;
+	return ((c >= 'A' && c <= 'Z') ? 1 : 0);
 }
